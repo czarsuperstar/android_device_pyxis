597ec2425179554062593402990e3f34e722b4af
Rodney Etienne <czarsuperstar@gmail.com>
2020-08-27 08:40:31 -0400

    First commit

---
+++ bootleg_pyxis.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_WIFI_EXT := true
+$(call inherit-product, vendor/gapps/config.mk)
+
+# Inherit from pyxis device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := pyxis
+PRODUCT_NAME := bootleg_pyxis
+PRODUCT_MODEL :=  Mi 9 Lite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="pyxis" \
+    TARGET_DEVICE="pyxis"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+
+# Build FP to be picked by both system and vendor
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
