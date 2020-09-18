c1ad6ab634388c602e01aa896fcd3eb16afb3c08
Rodney Etienne <czarsuperstar@gmail.com>
2020-09-18 02:28:13 -0400

    Update and rename zenx_pyxis.mk to freaky_pyxis.mk

--- freaky_pyxis.mk
+++ freaky_pyxis.mk
@@ -5,21 +5,21 @@
 #
 
 # Inherit some common zenx stuff.
-$(call inherit-product, vendor/zenx/config/common_full_phone.mk)
-ZENX_BUILD_TYPE := PiCzarro
+$(call inherit-product, vendor/freaky/config/common_full_phone.mk)
+
 TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_WIFI_EXT := true
-##$(call inherit-product, vendor/gapps/config.mk)
+##$(call inherit-product, vendor/gapps/
 
 # Inherit from pyxis device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := pyxis
-PRODUCT_NAME := zenx_pyxis
+PRODUCT_NAME := freaky_pyxis
 PRODUCT_MODEL :=  Mi 9 Lite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -28,8 +28,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := pyxis
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
-# Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+
+
+
+
